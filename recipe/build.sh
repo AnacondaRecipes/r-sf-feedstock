af1c1031baac1057f6107360aff82c3f8708b29a
Ray Donnelly <mingw.android@gmail.com>
2018-02-25 09:33:25 +0000

    Workaround poor configure.ac code that tries to link PROJ.4/GDAL

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# configure.ac has a hardcoded way to test for GDAL in PROJ.4:
+# ${CXX} ${CPPFLAGS} -o gdal_proj gdal_proj.cpp ${LIBS}
+export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,$PREFIX/lib"
 export PROJ_LIB=${PREFIX}/share/proj
 $R CMD INSTALL --build .
