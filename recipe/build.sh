91941b51f838a198d8e52d975f9ab20f2d6ddad6
Ray Donnelly <mingw.android@gmail.com>
2018-07-21 05:09:42 -0700

    Fix gdal-config configure tests on macOS to force c++14

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# configure.ac has a hardcoded way to test for GDAL in PROJ.4:
-# ${CXX} ${CPPFLAGS} -o gdal_proj gdal_proj.cpp ${LIBS}
-export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,$PREFIX/lib"
-export PROJ_LIB=${PREFIX}/share/proj
+# This is just to get around a configure failure when trying to link to gdal.
+if [[ ${target_platform} == osx-64 ]]; then
+  export CXX="${CXX} --std=c++14"
+fi
 $R CMD INSTALL --build .
