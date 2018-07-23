fbb7599da2a90abe77193c225db52a16280726b6
Ray Donnelly <mingw.android@gmail.com>
2018-07-23 13:16:47 +0000

    'Fix' configure proj4 rpath issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,8 @@
 # This is just to get around a configure failure when trying to link to gdal.
 if [[ ${target_platform} == osx-64 ]]; then
   export CXX="${CXX} --std=c++14"
+elif [[ ${target_platform} =~ .*linux.* ]]; then
+  # This is to keep configure happy when it compiles a test proj4 project. Urgh.
+  export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
 $R CMD INSTALL --build .
